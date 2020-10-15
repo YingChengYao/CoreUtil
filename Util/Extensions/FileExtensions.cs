72a8f9b19e0b4b326189a30052badbfcadf1fcd2
YingChengYao <35021489+YingChengYao@users.noreply.github.com>
2020-10-15 17:10:07 +0800

    添加扩展

---
+++ Util/Extensions/FileExtensions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Util.Extensions
+{
+    public static class FileExtensions
+    {
+        /// <summary>
+        /// 拷贝文件
+        /// </summary>
+        /// <param name="orignFile">原始文件</param>
+        /// <param name="newFile">新文件路径</param>
+        public static void FileCoppy(string orignFile, string newFile)
+        {
+            if (orignFile.IsNullOrEmpty())
+            {
+                throw new ArgumentException(orignFile);
+            }
+            if (newFile.IsNullOrEmpty())
+            {
+                throw new ArgumentException(newFile);
+            }
+            System.IO.File.Copy(orignFile, newFile, true);
+        }
+        /// <summary>
+        /// 删除文件
+        /// </summary>
+        /// <param name="path">路径</param>
+        public static void FileDel(string path)
+        {
+            if (path.IsNullOrEmpty())
+            {
+                throw new ArgumentException(path);
+            }
+            System.IO.File.Delete(path);
+        }
+        /// <summary>
+        /// 移动文件
+        /// </summary>
+        /// <param name="orignFile">原始路径</param>
+        /// <param name="newFile">新路径</param>
+        public static void FileMove(string orignFile, string newFile)
+        {
+            if (orignFile.IsNullOrEmpty())
+            {
+                throw new ArgumentException(orignFile);
+            }
+            if (newFile.IsNullOrEmpty())
+            {
+                throw new ArgumentException(newFile);
+            }
+            System.IO.File.Move(orignFile, newFile);
+        }
+        //创建路径
+        public static void CreatePath(string FilePath)
+        {
+            if (!Directory.Exists(FilePath))
+            {
+                Directory.CreateDirectory(FilePath);
+            }
+        }
+        //创建文件
+        public static void CreateFile(string FilePath)
+        {
+            if (!File.Exists(FilePath))
+            {
+                FileStream fs = File.Create(FilePath);
+                fs.Close();
+            }
+        }
+    }
+}
---
+++ Util/Extensions/ObjectExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Util.Extensions
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// 判断对象是否为空
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static bool IsNull(this object obj)
+        {
+            return obj == null;
+        }
+
+        /// <summary>
+        /// 判断对象是否不为空
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static bool IsNotNull(this object obj)
+        {
+            return obj != null;
+        }
+
+        public static T IsNotNull<T>(T value, string parameterName)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(parameterName);
+            }
+
+            return value;
+        }
+
+        public static T NotNull<T>(T value, string parameterName, string message)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(parameterName, message);
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="obj"></param>
+        public static void ThrowIfNull(this object obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="obj"></param>
+        public static void ThrowIfNull(this object obj, string message)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj), message);
+            }
+        }
+    }
+}
